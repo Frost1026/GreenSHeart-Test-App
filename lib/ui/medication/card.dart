784b8132cc53137b439e7c698a111fcdf75ed6a5
Chang Yi Zhong <yizhong.1026@gmail.com>
2024-10-13 11:00:15 +0800

    make medication card into buttons

--- lib/ui/medication/card.dart
+++ lib/ui/medication/card.dart
@@ -11,14 +11,82 @@ class MedicationCard extends StatefulWidget {
 }
 
 class _MedicationCardState extends State<MedicationCard> {
+    void _onCardPressed() {
+        print(widget.medication.name);
+    }
+
     @override
     Widget build(BuildContext context) {
-        return Card(
-            child: Column(
-                children: [
-                    Text(widget.medication.name),
-                    Text(widget.medication.dosage),
-                ],
+        return ElevatedButton(
+            onPressed: _onCardPressed,
+            style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+                ),
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                    children: [
+                        Expanded(
+                            flex: 1,
+                            child: Text(
+                                widget.medication.name,
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                            )
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                    Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                            const Text(
+                                                'Dose: ',
+                                                style: TextStyle(
+                                                    fontSize: 16.0,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                            ),
+                                            Text(
+                                                widget.medication.dosage,
+                                                style: const TextStyle(
+                                                    fontSize: 16.0
+                                                ),
+                                            ),
+                                        ],
+                                    ),
+                                    Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                            const Text(
+                                                'Time: ',
+                                                style: TextStyle(
+                                                    fontSize: 16.0,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                            ),
+                                            Text(
+                                                widget.medication.time,
+                                                style: const TextStyle(
+                                                    fontSize: 16.0
+                                                ),
+                                            ),
+                                        ],
+                                    )
+                                ],
+                            ),
+                        ),
+                    ],
+                ),
             ),
         );
     }
